d00da68b4dd209e7ac9fb8f5932e30102362bacb
ayushbisen <bisenayush70@gmail.com>
2024-05-03 00:48:37 +0530

    UI changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'unit-converter';
+  pxValue = 16;
+  remValue = 1;
+  base = 16;
+
+  public onBaseChange(event: any) {
+    this.remValue = this.pxValue / event.target.value;
+  }
+
+  public onPXChange(event: any) {
+    this.remValue = event.target.value / this.base;
+
+  }
+  public onREMChange(event: any) {
+    this.pxValue = event.target.value * this.base;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    FormsModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
